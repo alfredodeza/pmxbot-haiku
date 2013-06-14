1784a23ae1bfe003509c0254141f6b58f7b4bdd7
Alfredo Deza <alfredo@deza.pe>
2013-06-13 20:46:22 -0400

    attempt to not use self.lib

--- pmxbot_haiku/models.py
+++ pmxbot_haiku/models.py
@@ -43,8 +43,9 @@ class MongoDBHaikus(Haikus, storage.MongoDBStorage):
     def _make_results(self, regex=None):
         # have tried with 'text' in find_opts but not dice
         if regex:
-            find_opts = {'library': self.lib,
-                         'message': {'$regex': self._make_term_pattern(regex)}}
+            find_opts = {'text': {'$regex': self._make_term_pattern(regex)}}
+            #find_opts = {'library': self.lib,
+            #             'message': {'$regex': self._make_term_pattern(regex)}}
         else:
             find_opts = dict(library=self.lib)
         return [
